ddab911ff25ac502670e27997a1746e44f47da66
said-alisic <saidalisic@gmail.com>
2021-05-02 23:37:15 +0200

    .

--- dsl-app/apps/dsl-app/src/app/pages/attendance/attendance.tsx
+++ dsl-app/apps/dsl-app/src/app/pages/attendance/attendance.tsx
@@ -12,79 +12,7 @@ export interface AttendancesProps {}
 function Attendances(props: AttendancesProps) {
   const { Column } = Table;
 
-  const [attendances, setAttendances] = useState<Attendance[]>([])
-  const [classes, setClasses] = useState<Class[]>([])
   
-  const { Option } = Select;
-
-  const layout = {
-    labelCol: { span: 10 },
-    wrapperCol: { span: 8 },
-  };
-  const tailLayout = {
-    wrapperCol: { offset: 10, span: 8 },
-  };
-  
-  useEffect(() => {
-    getClasses().then(res => {
-      setClasses(res.data)
-    })
-  }, [])
-
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
-
-  const handleSubmit = (values) => {
-    getAttendancesByClass(values.class_id)
-    .then(res => {
-      setAttendances(res.data)  
-    })
-  }
-
-  return (
-    <>
-      <Form
-          {...layout}
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={(values) => handleSubmit(values)}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item
-            label="Class"
-            name="class_id"
-            rules={[{ required: true, message: 'Please select a class!' }]}
-          >
-            <Select
-                placeholder="Select a class"
-                allowClear
-              >
-              {classes.map((classItem, index) => {
-                return (
-                  <Option value={classItem.id} key={index}>{classItem.name}</Option>
-                  )
-              })}
-            </Select>
-          </Form.Item>
-          <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit">
-              Check Attendance
-            </Button>
-          </Form.Item>
-        </Form>
-      <Table dataSource={attendances} rowKey="id" pagination={{defaultPageSize: 10, hideOnSinglePage: true}} >
-      <Column title="Class Name" dataIndex="name" key="name" />
-      <Column title="Student Email" dataIndex="email" key="email" />
-      <Column title="First Name" dataIndex="first_name" key="first_name" />
-      <Column title="Last Name" dataIndex="last_name" key="last_name" />
-      <Column title="Present" dataIndex="present" key="present" />
-      <Column title="Attendance Note" dataIndex="description" key="description" />
-      <Column title="Date" dataIndex="date" key="date" />
-      <Column title="Time Slot" dataIndex="timeslot" key="timeslot" />
-    </Table>
-  </>
-  );
 }
 
 export default Attendances;
