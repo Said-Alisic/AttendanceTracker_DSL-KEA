c34b48f48897d270e1f685f07ebaeb7256bff450
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-05-24 17:30:54 +0200

    .json responses refactored -- containing message

--- dsl-app/apps/api/src/app/auth/authParams.ts
+++ dsl-app/apps/api/src/app/auth/authParams.ts
@@ -1,27 +1,33 @@
-import { 
-  Request, 
-  Response, 
-  NextFunction 
+import {
+  Request,
+  Response,
+  NextFunction
 } from "express";
 
 /* eslint-disable consistent-return */
 const verifyIdParam = (req: Request, res: Response, next: NextFunction) => {
   if (!req.params.id.match(/^[0-9]*$/)) {
-    return res.status(404).json('Wrong id format. Try again.');
+    return res.status(404).json({
+      message: 'Wrong id format. Try again.'
+    });
   }
   next();
 };
 
 const verifyCodeClassIdParams = (req: Request, res: Response, next: NextFunction) => {
   if (!req.params.codeId.match(/^[0-9]*$/) || !req.params.classId.match(/^[0-9]*$/)) {
-    return res.status(404).json('Wrong id format. Try again.');
+    return res.status(404).json({
+      message: 'Wrong id format. Try again.'
+    });
   }
   next();
 }
 
 const verifyCodeStringParam = (req: Request, res: Response, next: NextFunction) => {
   if (!req.params.codeString.match(/^[0-9a-zA-Z]*$/)) {
-    return res.status(404).json('Wrong code string format. Try again.');
+    return res.status(404).json({
+      message: 'Wrong code string format. Try again.'
+    });
   }
   next();
 }
@@ -33,4 +39,3 @@ const authParams = {
 };
 
 export default authParams;
-  
